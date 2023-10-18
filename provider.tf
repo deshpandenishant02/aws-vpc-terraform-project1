22ffb6e7135942d52979d1e4fcb932b57d0e27e1
deshpandenishant02 <deshpandenishant02@gmail.com>
2023-10-18 21:53:55 +0530

    first-git-commit

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region = "ap-south-1a"
+    access_key = "AKIAWI5XFGWLSQCAYQXS"
+    secret_key = "huGmuhgNzF6KZsJPyOtCRUDBMZUr/+rIjQaXZk9A"
+}
---
+++ resource.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "example_vpc" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        name = "example_vpc"
+    }
+}
+
